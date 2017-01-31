f832b046958475589bc7a635566e60ff2e1f9056
takkkkk <git@github.com:takkkkk/sample_app.git>
2017-01-31 05:57:23 +0000

    nokogiriを使った記事タイトル・URLの取得、それらをView上で表示

--- ske48/app/controllers/article_controller.rb
+++ ske48/app/controllers/article_controller.rb
@@ -4,21 +4,32 @@ require 'open-uri'
 class ArticleController < ApplicationController
 
   def index
-    @articles = Article.all
-    @article = Article.new
+    # @articles = Article.all
+    # @article = Article.new
 
     doc = Nokogiri::HTML(open('http://www.ske48matoeme.com'))
     x = doc.xpath('//h2')
 
+    # 記事のタイトルとURLを抜き出してDBにないものは保存する
     x.xpath('.//a').each do |title|
+      @article = Article.new
       @article[:title] = title.inner_text
       @article[:url] = title[:href]
+      i = 0
+      @articles.each do |t|
+        if t.url == @article[:url]
+          i =+ 1
+        end
+      end
+
+      if i==0
+        @article.save
+      end
     end
 
-    binding.pry
+    @articles = Article.all
 
-    @article.save
-
+    # binding.pry
 
   end
 
